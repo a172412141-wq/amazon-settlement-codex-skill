eeb4c61efb72aef2f3ad4a532fe989ec7217cfd0
1sme <a172412141@gmail.com>
2026-05-30 17:06:25 +0800

    Add Windows apply helper

---
+++ run_apply.bat
@@ -0,0 +1,8 @@
+@echo off
+set WORKBOOK=%1
+set PDFDIR=%2
+set OUTPUT=%3
+if "%WORKBOOK%"=="" set WORKBOOK=.\template.xlsx
+if "%PDFDIR%"=="" set PDFDIR=.\pdfs
+if "%OUTPUT%"=="" set OUTPUT=.\template_filled.xlsx
+python .agents\skills\amazon-settlement-xlsx\scripts\run.py --workbook "%WORKBOOK%" --pdf-dir "%PDFDIR%" --output "%OUTPUT%" --mapping .agents\skills\amazon-settlement-xlsx\assets\store_mapping.csv
